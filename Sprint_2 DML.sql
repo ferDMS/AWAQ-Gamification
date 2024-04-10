651930d13a8dd603d7dfbc0f06b5af4546cde50a
Daniel Monroy <fernandodms04@gmail.com>
2024-04-10 02:18:41 -0500

    Inserts de datos del juego

--- Sprint_2 DML.sql
+++ Sprint_2 DML.sql
@@ -6,6 +6,7 @@ USE sprint2;
 -- Tablas para guardar información de usuarios y admins
 -- Para autentificar en el log in y guardar información personal
 -- ------------------------------------------------------------------------------------------
+
 -- Información de los biomonitores / usuarios
 INSERT INTO usuarios(nombre, apellido, genero, pais, ciudad, fechaNacimiento, correo, pass_word, lastLogin) VALUES
 	('Fernando', 'Monroy', 'H', 'México', 'Monterrey', '2004-01-15', 'fernando@awaq.org', 'monroy', '2024-03-13 14:04:00'),
@@ -18,72 +19,112 @@ INSERT INTO usuarios(nombre, apellido, genero, pais, ciudad, fechaNacimiento, co
 INSERT INTO admin(nombre, apellido, correo, pass_word) VALUES
 	('Angélica', 'Lozano', 'angelica@awaq.org', 'lozano'),
     ('José', 'Serna', 'jose@awaq.org', 'serna');
+   
 -- ------------------------------------------------------------------------------------------
 
-/*
+   
 -- ------------------------------------------------------------------------------------------
 -- Tablas para guardar información básica para el funcionamiento del juego
 -- Esta información se usa dentro de las mecánicas del juego y es vital para su correcto funcionamiento
 -- Esta información es independiente del usuario, pues únicamente define cómo es el juego
 -- ------------------------------------------------------------------------------------------
+  
 -- Herramientas que se pueden usar dentro del juego
-create table if not exists herramientas(
-	herramienta_id int not null auto_increment,
-	nombre_herramienta varchar(50) not null,
-	descripcion varchar(300),
-	xp_desbloqueo int NOT NULL,
-	primary key (herramienta_id)
-);
+INSERT INTO herramientas(nombre_herramienta, descripcion, xp_desbloqueo) VALUES
+	('Lupa', 'Descripción de uso de lupa', 0),
+	('Binoculares', 'Descripción de uso de binoculares', 0),
+	('Cámara trampa', 'Descripción de uso de cámara trampa', 7500),
+	('Caja trampa', 'Descripción de uso de caja trampa', 7500),
+	('Red', 'Descripción de uso de red de nieba', 20000),
+	('Linterna', 'Descripción de uso de linterna', 20000);
+
 -- Regiones de donde pueden provenir especies del juego
-create table if not exists regiones(
-	region_id int not null auto_increment,
-	nombre_region varchar(50) not null,
-	primary key (region_id)
-);
+INSERT INTO regiones(nombre_region) VALUES
+	('Cordillera de los Andes'),
+	('Bosques nublados'),
+	('Bosques húmedos'),
+	('Bosques húmedos de tierras bajas'),
+	('Selva amazónica'),
+	('Selvas tropicales'),
+	('Páramos');
+
 -- Tipos de especies existentes dentro del juego (flora y fauna)
-CREATE TABLE IF NOT EXISTS tipos_de_especies(
-	especie_tipo_id int NOT NULL AUTO_INCREMENT,
-	tipo VARCHAR(30) NOT NULL,
-	PRIMARY KEY (especie_tipo_id),
-	UNIQUE KEY (tipo)
-);
+INSERT INTO tipos_de_especies(tipo) VALUES ('Flora'), ('Fauna');
+
 -- Lista de especies que pueden aparecer dentro del juego
-create table if not exists especies(
-	especie_id int not null auto_increment,
-	
+INSERT INTO especies(
 	-- Información básica de la especie
-	nombre_especie varchar(30) not null,
-	nombre_cientifico varchar(30) not null,
-	region_id int not null,
-	especie_tipo_id int NOT NULL,
-	
+	nombre_especie,
+	nombre_cientifico,
+	region_id,
+	especie_tipo_id,
 	-- Información necesaria para mecánicas de videojuego
-	xp_desbloqueo int not null,
-	xp_registro int NOT NULL,
-	xp_captura int DEFAULT NULL, 
-	herramienta_id int not null,
-	
-	primary key (especie_id),
-	constraint tde_region_id_fk foreign key (region_id) references regiones(region_id),
-	CONSTRAINT tde_especie_tipo_id_fk FOREIGN KEY (especie_tipo_id) REFERENCES tipos_de_especies(especie_tipo_id),
-	CONSTRAINT tde_herramienta_id_fk FOREIGN KEY (herramienta_id) REFERENCES herramientas(herramienta_id)
-);
+	xp_desbloqueo,
+	xp_registro,
+	xp_captura,
+	herramienta_id
+) 
+VALUES
+	('Cóndor Andino', 'Vultur Gryphus', 1, 2, 0, 200, 600, 2),
+	('Oso de Anteojos', 'Tremarctos Ornatus', 2, 2, 2500, 375, 750, 3),
+	('Lagarto Punteado', 'Diploglossus Millepunctatus', 3, 2, 7500, 475, 950, 4),
+	('Tucán Pechiblanco', 'Ramphastidae Tucanus', 4, 2, 20000, 500, 1000, 5),
+	('Polilla Esfinge Tersa', 'Xylophanes Tersa', 2, 2, 30000, 250, 1500, 6),
+	('Tití Ornamentado', 'Callicebus Ornatus', 5, 2, 50000, 675, 2000, 4),
+	('Ave del Paraíso', 'Heliconia Latispatha', 6, 1, 0, 500, NULL, 1),
+	('Orquídea flor de Mayo', 'Cattleya trianae', 2, 1, 2500, 600, NULL, 1),
+	('Arbusto de la mermelada', 'Streptosolen jamesonii', 2, 1, 7500, 750, NULL, 1),
+	('Palma de Cera del Quindío', 'Ceroxylon Quindiuense', 2, 1, 20000, 1000, NULL, 1),
+	('Frailejones', 'Espeletia Pycnophylla', 7, 1, 30000, 1500, NULL, 1),
+	('Arbol de Cacao', 'Theobroma cacao', 5, 1, 50000, 2000, NULL, 1);
+
 -- Lista de desafíos que deben ser superados a través del juego
-CREATE TABLE IF NOT EXISTS desafios(
-	desafio_id int NOT NULL AUTO_INCREMENT,
-	xp_desbloqueo int NOT NULL,
-	xp_completar int,
-	xp_fallar int,
-	PRIMARY KEY (desafio_id)
-);
+INSERT INTO desafios(xp_desbloqueo, xp_completar, xp_fallar) VALUES
+	(0, 0, 0),
+	(6000, 1500, 1500),
+	(17000, 3000, 2000),
+	(42000, 8000, 7000),
+	(85000, 15000, 10000);
+
 -- Los desafíos desbloquean herramientas
 -- Debemos detallar cuales desafíos desbloquean cuales herrramientas
-CREATE TABLE IF NOT EXISTS desafios_herramientas(
-	desafio_id int NOT NULL,
-	herramienta_id int NOT NULL,
-	PRIMARY KEY (desafio_id, herramienta_id),
-	CONSTRAINT dh_desafio_id_fk FOREIGN KEY (desafio_id) REFERENCES desafios(desafio_id),
-	CONSTRAINT dh_herramienta_id_fk FOREIGN KEY (herramienta_id) REFERENCES herramientas(herramienta_id)
-);
+INSERT INTO desafios_herramientas(desafio_id, herramienta_id) VALUES
+	(1, 1),
+	(1, 2),
+	(2, 3),
+	(2, 4),
+	(3, 5),
+	(3, 6);
 -- ------------------------------------------------------------------------------------------
+
+/*
+-- ------------------------------------------------------------------------------------------''
+-- Tablas para guardar la información del progreso de los usuarios mientras juegan
+-- Estas tablas unifican las dos secciones anteriores de usuarios y mecánicas del juego
+-- ------------------------------------------------------------------------------------------
+-- Tabla para salvar el progreso del usuario en materia de cada captura / avistamiento de especies
+-- Óptimamente ingresaríamos datos a esta table con inserts de múltiples entradas correspondientes a una sesión de juego
+CREATE TABLE IF NOT EXISTS progreso_especies (
+	progreso_id int NOT NULL AUTO_INCREMENT,
+	user_id int NOT NULL,
+	especie_id int NOT NULL,
+	fecha datetime NOT NULL,
+	PRIMARY KEY (progreso_id),
+	KEY (fecha),
+	CONSTRAINT pe_user_id_fk FOREIGN KEY (user_id) REFERENCES usuarios(user_id),
+	CONSTRAINT pe_especie_id_fk FOREIGN KEY (especie_id) REFERENCES especies(especie_id)
+);
+-- Tabla para salvar el progreso del usuario en materia de desafíos completados
+CREATE TABLE IF NOT EXISTS progreso_desafios(
+	progreso_id int NOT NULL AUTO_INCREMENT,
+	desafio_id int NOT NULL,
+	user_id int NOT NULL,
+	start_time datetime DEFAULT NULL,
+	end_time datetime DEFAULT NULL,
+	PRIMARY KEY (progreso_id),
+	KEY (start_time),
+	KEY	(end_time),
+	CONSTRAINT pd_desafio_id_fk FOREIGN KEY (desafio_id) REFERENCES desafios(desafio_id),
+	CONSTRAINT pd_user_id_fk FOREIGN KEY (user_id) REFERENCES usuarios(user_id)
+);
 */
