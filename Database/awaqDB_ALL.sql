fdec701ca42b863f00219d42c71b59d199805fbc
Daniel Monroy <fernandodms04@gmail.com>
2024-04-17 19:15:40 -0500

    Delete unnecessary procedures

--- Database/awaqDB_ALL.sql
+++ Database/awaqDB_ALL.sql
@@ -461,7 +461,7 @@ END//
 
 -- Reactivar de manera lógica a un usuario que previamente fue borrado (desactivado)
 DROP PROCEDURE IF EXISTS `undoDeleteUser`//
-CREATE PROCEDURE `deleteUser`(IN user_id_in INT)
+CREATE PROCEDURE `undoDeleteUser`(IN user_id_in INT)
 BEGIN
 	UPDATE usuarios SET isActive = 1 WHERE user_id = user_id_in;
 END//
@@ -526,57 +526,3 @@ COMMIT;
 -- ------------------------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------------------
 -- AWAQ_API_Procedures.sql
-
-
-
-
-
--- AWAQ_Dashboard_Procedures.sql
--- ------------------------------------------------------------------------------------------
--- ------------------------------------------------------------------------------------------
-
-USE awaqDB;
-
-START TRANSACTION;
-
-DELIMITER //
-
--- --------------------------------------------------------
--- Procedures para los dashboards desarrollados en Looker Studio
--- ESTOS PROCEDURES NO SE LLAMAN, únicamente se utilizan los querys como data source
--- --------------------------------------------------------
--- Para gráficas de horas por día
-DROP PROCEDURE IF EXISTS `allTimePerDay`//
-CREATE PROCEDURE `allTimePerDay`()
-BEGIN
-    SELECT
-    	SEC_TO_TIME(SUM(TIMESTAMPDIFF(SECOND, s.start_time, s.end_time))) AS `Tiempo`, 
-    	DATE(s.end_time) AS `Fecha`
-    FROM
-    	sesiones s
-    GROUP BY
-    	DATE(s.end_time);
-END//
--- Para gráficas de xp por fecha (días, horas, etc.)
-DROP PROCEDURE IF EXISTS `allXpPerDay`//
-CREATE PROCEDURE `allXpPerDay`()
-BEGIN
-	SELECT
-	    SUM(f.xp_exito) AS `XP Total`,
-	    DATE(fecha) AS `Fecha`
-	FROM
-	    progreso p
-	LEFT JOIN
-	    fuentes_xp f ON p.fuente_id = f.fuente_id
-	GROUP BY
-	    DATE(fecha);
-END//
--- --------------------------------------------------------
-
-DELIMITER ;
-
-COMMIT;
-
--- ------------------------------------------------------------------------------------------
--- ------------------------------------------------------------------------------------------
--- AWAQ_Dashboard_Procedures.sql
