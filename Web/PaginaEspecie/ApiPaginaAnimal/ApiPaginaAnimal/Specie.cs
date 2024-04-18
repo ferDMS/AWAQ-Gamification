879807af28b2ed69a2bce2b4cdb5476eb7d72dfb
Rodrigo López Murguía <rocco_lpz@hotmail.com>
2024-04-18 14:03:05 -0600

    Added Api/Page for Species Page
    La Api de momento se conecta a la base de datos local en SpeciesController.cs

---
+++ Web/PaginaEspecie/ApiPaginaAnimal/ApiPaginaAnimal/Specie.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiPaginaAnimal
+{
+    public class Specie
+    {
+        public string? nombre { get; set; }
+        public string? nombre_cientifico {  get; set; }
+        public string? descripcion {  get; set; }
+        public string? url_img {  get; set; }
+        public string? color { get; set; }
+        public string? tamagno {  get; set; }
+        public string? region {  get; set; }
+        public string? tipo {  get; set; }
+        public string? herramienta {  get; set; }
+
+    }
+}
---
+++ Web/PaginaEspecie/MostrarApiAnimal/MostrarApiAnimal/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+
+namespace MostrarApiAnimal.Pages
+{
+    public class IndexModel : PageModel
+    { 
+        public List<Specie> listaSpecies { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string url = "https://localhost:7100/species/Species";
+                var response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    listaSpecies = JsonConvert.DeserializeObject<List<Specie>>(jsonString);
+                }
+            }
+        }
+    }
+}
