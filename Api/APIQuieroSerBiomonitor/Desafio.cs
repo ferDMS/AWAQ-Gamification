0680930d65e853df43faf8b2c36ea3a0195082d3
Daniel Monroy <fernandodms04@gmail.com>
2024-04-19 00:07:36 -0500

    Change model structure to match new DB

--- Api/APIQuieroSerBiomonitor/Desafio.cs
+++ Api/APIQuieroSerBiomonitor/Desafio.cs
@@ -4,9 +4,6 @@ namespace APIQuieroSerBiomonitor
     public class Desafio : Fuente
     {
         public int DesafioId { get; set; }
-        public int XpDesbloqueo { get; set; }
-        public int XpExito { get; set; }
-        public int XpFallar { get; set; }
         public List<int>? HerramientaIds { get; set; } = new List<int>();
     }
 }
--- Api/APIQuieroSerBiomonitor/Fuente.cs
+++ Api/APIQuieroSerBiomonitor/Fuente.cs
@@ -6,6 +6,7 @@ namespace APIQuieroSerBiomonitor
         public int FuenteId { get; set; }
         public int XpDesbloqueo { get; set; }
         public int XpExito { get; set; }
+        public int XpFallar { get; set; }
     }
 }
 
