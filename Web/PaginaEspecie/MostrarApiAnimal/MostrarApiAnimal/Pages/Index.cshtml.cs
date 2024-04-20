dc61b79d3f45e92f8bce609b7248e0b6eeb2388b
Daniel Monroy <fernandodms04@gmail.com>
2024-04-20 02:29:47 -0500

    Add comments to code

--- Web/PaginaEspecie/MostrarApiAnimal/MostrarApiAnimal/Pages/Index.cshtml.cs
+++ Web/PaginaEspecie/MostrarApiAnimal/MostrarApiAnimal/Pages/Index.cshtml.cs
@@ -7,18 +7,33 @@ namespace MostrarApiAnimal.Pages
 {
     public class IndexModel : PageModel
     { 
+        // List of species that will be filled from the API call
         public List<Especie> listaEspecies { get; set; }
 
         public async Task OnGetAsync()
         {
             using (var httpClient = new HttpClient())
             {
-                string url = "https://localhost:7044/QSBWeb/especies";
-                var response = await httpClient.GetAsync(url);
-                if (response.IsSuccessStatusCode)
+                // Try to get something from the API
+                try
                 {
-                    var jsonString = await response.Content.ReadAsStringAsync();
-                    listaEspecies = JsonConvert.DeserializeObject<List<Especie>>(jsonString);
+                    string url = "https://localhost:7044/QSBWeb/especies";
+                    var response = await httpClient.GetAsync(url);
+                    // If we are succesfull and obtain data from the API
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Parse the JSON content from the response and match it with our species model
+                        // The parsing is actually done into multiple models but the deseralization is very smart about it (apparently)
+                        var jsonString = await response.Content.ReadAsStringAsync();
+                        listaEspecies = JsonConvert.DeserializeObject<List<Especie>>(jsonString);
+                    }
+                }
+
+                // If there's an error with the HTTP request, then create a default, empty, list of species
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    listaEspecies = new List<Especie>();
                 }
             }
         }
